3ef5bcf906bfae7f3eccc758964ca42e10e5ba78
michael onyeije <monyeije16@saintpeters.edu>
2020-09-24 17:48:24 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,31 @@
+def using_concat(array, element)
+  return array.concat(element)
+
+end
+
+def using_insert(array, element)
+  help = array.insert(4,element)
+  return help
+  print help
+
+end
+
+def using_uniq(array)
+  return array.uniq()
+
+end
+
+def using_flatten(array)
+  return array.flatten()
+  
+end
+
+def using_delete(array,string)
+  return array.delete(string)
+
+end
+
+def using_delete_at(array,int)
+  return array.delete_at(int)
+
+end
